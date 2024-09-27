0d0c392329bb06164b499f13b2fb9fda83c12b89
divya12112003 <rahangdaledivya78@gmail.com>
2024-09-27 14:11:04 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -6,16 +6,7 @@
  #       """
    #     pass
 
-def isValid(s: str) -> bool:
-    # Dictionary to match opening and closing brackets
-    bracket_map = {')': '(', ']': '[', '}': '{'}
-    # Stack to keep track of opening brackets
-    stack = []
-    
-    # Iterate over each character in the string
-    for char in s:
-        # If it's a closing bracket
-        if char in bracket_map
+
 
 
 
