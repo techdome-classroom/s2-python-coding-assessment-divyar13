b45e0bfed4160431c5053a0eacf4599d297c096c
divya12112003 <rahangdaledivya78@gmail.com>
2024-09-27 14:21:45 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,10 +1,46 @@
+#class Solution(object):
+#    def romanToInt(self, s):
+#       """
+ #       :type s: str
+  #      :rtype: int
+  #      """
+  #      pass
+
+# class Solution(object):
+#     def romanToInt(self, s):
+#         """
+#         :type s: str
+#         :rtype: int
+#         """
+#         pass
+
 class Solution(object):
     def romanToInt(self, s):
         """
         :type s: str
         :rtype: int
         """
-        pass
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        for i in range(len(s)):
+            if i < len(s) - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
+                total -= roman_map[s[i]]
+            else:
+                total += roman_map[s[i]]
+        
+        return total
 
-
+# solution = Solution()
+# print(solution.romanToInt("III"))      
+# print(solution.romanToInt("LVIII"))     
+# print(solution.romanToInt("MCMXCIV"))
 
